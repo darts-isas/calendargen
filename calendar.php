03239fb5cefe36a1994bdbee84c0185e7246e584
tanakanonoka <118522411+tanakanonoka@users.noreply.github.com>
2023-02-25 05:26:58 +0000

    update php

--- calendar.php
+++ calendar.php
@@ -89,7 +89,7 @@ if ($aspect == "1609") {
     //   縦組の場合
         else if ($arrangement == "縦") {
         if ($position == "左上" || $position == "左下") {
-            $px = 0;
+            $px = 60;
             $py = (1080 - (1080 * $size)) / 2;
         } else if ($position == "中央") {
             $px = (1920 - (500 * $size)) / 2;
